edb8cb8ef017ea328e310c6fa12518cc88cdcccd
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2021-02-04 17:25:03 -0500

    datacube with airflow

---
+++ datacube-1.8.2-airflow-2.0.0/airflow/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Reference: https://stackoverflow.com/questions/41451159/how-to-execute-a-script-when-i-terminate-a-docker-container
+
+#Define cleanup procedure
+cleanup() {
+    echo "Container stopped, performing cleanup..."
+    rm -rf /home/datacube/airflow/{*.out,*.pid,*.py,*.err,*.log}
+    rm -rf /home/datacube/airflow/{unittests.cfg,logs}
+}
+
+#Cleanup at the begining
+cleanup
+
+#Trap SIGTERM
+trap 'cleanup' SIGTERM
+
+#Execute a command
+
+airflow db init
+
+airflow users create \
+      --username admin \
+      --firstname Peter \
+      --lastname Parker \
+      --role Admin \
+      --email spiderman@superhero.org \
+      --password admin
+
+airflow webserver -D &
+
+airflow celery flower -D &
+
+airflow scheduler
